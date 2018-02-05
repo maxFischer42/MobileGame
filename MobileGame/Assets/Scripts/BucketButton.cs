cfe1b577b9ffee5a9078e0f167caf7782a7fee0f
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-02-05 13:09:49 -0800

    fixed stuff

--- MobileGame/Assets/Scripts/BucketButton.cs
+++ MobileGame/Assets/Scripts/BucketButton.cs
@@ -4,14 +4,51 @@ using UnityEngine;
 
 public class BucketButton : MonoBehaviour {
 	public BucketPuzzle Buck;
-
+	public float multiplier = 2.0;
 	public int direction;
+	public Sprite Disabled;
+	public Sprite Enabled;
 	//0 = up 1 = down
 	//2 = left 3 = right
 
 	void OnTriggerEnter2D(Collider2D Coll)
 	{
+		if (Coll.gameObject.name == "Feet") {
+			switch (direction) {
+			case 0:
+				Buck.Up (multiplier);
+				break;
+			case 1:
+				Buck.Down (multiplier);
+				break;
+			case 2:
+				Buck.Left (multiplier);
+				break;
+			case 3:
+				Buck.Right (multiplier);
+				break;
+			}
+		}
+	}
 
+	void OnTriggerEnter2D(Collider2D Coll)
+	{
+		if (Coll.gameObject.name == "Feet") {
+			switch (direction) {
+			case 0:
+				Buck.Up (multiplier);
+				break;
+			case 1:
+				Buck.Down (multiplier);
+				break;
+			case 2:
+				Buck.Left (multiplier);
+				break;
+			case 3:
+				Buck.Right (multiplier);
+				break;
+			}
+		}
 	}
 
 
