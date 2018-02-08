480966843e6a1ed8ac321bd3e9f346002821870d
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-02-08 13:05:49 -0800

    Finished base level 3

--- MobileGame/Assets/Scripts/GravityChange.cs
+++ MobileGame/Assets/Scripts/GravityChange.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class GravityChange : MonoBehaviour {
 
-	public bool direction;
 	public GameObject Feet;
 	public GameObject AboveFeet;
 
 		void OnTriggerEnter2D(Collider2D Coll)
 	{
-		Rigidbody2D Rig = Coll.GetComponent<Rigidbody2D> ();
-		switch (direction) {
-		case false:
-			Rig.gravityScale = Mathf.Abs (Rig.gravityScale);
-			break;
-		case true:
+		if (Coll.gameObject.tag != "Bucket" || Coll.gameObject.layer != 8) {
+			Rigidbody2D Rig = Coll.GetComponent<Rigidbody2D> ();
 			Rig.gravityScale = (Mathf.Abs (Rig.gravityScale)) * -1;
 			Feet.SetActive (false);
 			AboveFeet.SetActive (true);
-			break;
+				
+
+		} else if(Coll.gameObject.tag == "Bucket"){
+			Debug.Log ("Boosted");
+			Rigidbody2D Rig = Coll.GetComponent<Rigidbody2D> ();
+			Rig.AddForce (new Vector2(0, 350));
+
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D Coll){
-		Rigidbody2D Rig = Coll.GetComponent<Rigidbody2D> ();
-		Rig.gravityScale = 3;
-		Feet.SetActive (true);
-		AboveFeet.SetActive (false);
+		if (Coll.gameObject.tag != "Bucket" && Coll.gameObject.layer != 8) {
+			Rigidbody2D Rig = Coll.GetComponent<Rigidbody2D> ();
+			Rig.gravityScale = 3;
+			Feet.SetActive (true);
+			AboveFeet.SetActive (false);
+		}
 	}
 
 }
---
+++ MobileGame/Assets/Scripts/dontchangegrav.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontchangegrav : MonoBehaviour {
+
+	private Rigidbody2D Rig;
+	// Use this for initialization
+	void Start () {
+		Rig = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rig.gravityScale = 1;
+	}
+}
