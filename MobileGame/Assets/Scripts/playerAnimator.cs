a30a7ca07f818d75a21c5811a43d0e35c31aa642
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-02-12 13:59:29 -0800

    stuff

--- MobileGame/Assets/Scripts/playerAnimator.cs
+++ MobileGame/Assets/Scripts/playerAnimator.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class playerAnimator : MonoBehaviour {
 
+	public RuntimeAnimatorController Move;
+	public RuntimeAnimatorController Attack;
+
+	private SwordBehavior sword;
+	private Rigidbody2D Rig;
+	private Animator Anim;
+	private SpriteRenderer Sprt;
+
 	// Use this for initialization
 	void Start () {
-		
+		Rig = GetComponent<Rigidbody2D> ();
+		Anim = GetComponent<Animator> ();
+		sword = GetComponent<SwordBehavior> ();
+		Sprt = GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+
+		float x = Rig.velocity.x;
+
+
+		if (x > 0) {
+			Sprt.flipX = false;
+		} else if (x < 0) {
+			Sprt.flipX = true;
+		}
+		x = Mathf.Abs (x);
+		if (x > 1) {
+			Anim.enabled = true;
+
+			if (sword.attacking) {
+				Anim.runtimeAnimatorController = Attack;
+			}else 
+				if (sword.attacking != true) {
+					Anim.runtimeAnimatorController = Move;
+				}
+		} else if (x == 0 || sword.attacking != true) {
+			Anim.enabled = false;
+
+		}
+
+
 	}
 }
