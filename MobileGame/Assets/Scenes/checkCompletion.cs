64c906c0eeb4bc667c0a2195610ffe2147a073db
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-02-15 12:10:37 -0800

    AAA

---
+++ MobileGame/Assets/Scenes/checkCompletion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkCompletion : MonoBehaviour {
+
+	public Button Med;
+	public Button Hard;
+
+	// Use this for initialization
+	void Start () {
+		int a = PlayerPrefs.GetInt ("diffUnlock");
+		if (2 > a && a > 0) {
+			Med.interactable = true;
+		}
+		if (a >= 2) {
+			Hard.interactable = true;
+		}
+	}
+
+
+}
---
+++ MobileGame/Assets/Scenes/load.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class load : MonoBehaviour {
+
+	public string levelToLoad;
+	public int diff;
+
+	public void press()
+	{
+		SceneManager.LoadScene (levelToLoad);
+		switch (diff) {
+		case 0:
+			PlayerPrefs.SetInt ("Difficulty") = 1;
+			break;
+
+		}
+	}
+	
+
+}
